25e3ca47b668cc5b9f4762e4dd048dbd2e7416b3
sl02297 <sl02297@surrey.ac.uk>
2024-09-27 00:32:00 +0100

    removed unused variable and added imports

--- src/code/client.java
+++ src/code/client.java
@@ -1,5 +1,9 @@
 package code;
 
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
 public class client {
 
 	public static void main(String[] args) {
--- src/code/server.java
+++ src/code/server.java
@@ -4,7 +4,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server {
-	private int port = 5054;
 	
 	public static void main(String[] args) throws Exception {
 		try {
