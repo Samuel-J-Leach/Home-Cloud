1ee6e49b0625e8434a94eeae342212ee2c5021a4
sl02297 <sl02297@surrey.ac.uk>
2024-09-27 11:36:26 +0100

    added network code to client

--- src/code/client.java
+++ src/code/client.java
@@ -1,14 +1,29 @@
 package code;
 
 import java.net.Socket;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 
 public class client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			Socket socket = new Socket("127.0.0.1", 5054);//replace with server IP
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			String message;
+			do {
+				message = input.readLine();
+				out.writeUTF(message);
+				out.flush();
+				System.out.println(in.readUTF());
+			} while (!message.equals("exit"));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }
