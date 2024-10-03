b156a124e4e0df3c88933aeb0e51ba5148eeae71
sl02297 <sl02297@surrey.ac.uk>
2024-10-04 00:24:17 +0100

    basic file transfer from server to client

--- src/code/Client.java
+++ src/code/Client.java
@@ -4,23 +4,62 @@ import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Client {
+	private static Socket socket;
+	private static DataInputStream in;
+	private static DataOutputStream out;
+	
+	private static void receiveFile(String filename, long length) throws Exception {
+		// TODO: write data to file
+		//File file = new File(filename);
+		//file.createNewFile();
+		int b;
+		for (long i=0; i<length; i++) {
+			b = in.read();
+			System.out.print(b);
+		}
+		if (!in.readUTF().equals("Server: transfer complete")) throw new Exception("something went wrong");
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		try {
 			Socket socket = new Socket("127.0.0.1", 5054);//replace with server IP
 			DataInputStream in = new DataInputStream(socket.getInputStream());
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 			String message;
+			String response;
+			String[] content;
 			do {
+				System.out.print(">");
 				message = input.readLine();
 				out.writeUTF(message);
 				out.flush();
-				System.out.println(in.readUTF());
+				response = in.readUTF();
+				System.out.println(response);
+				content = response.split(" ");
+				switch (content[1]) {
+				case "sending":
+					//receiveFile(content[2], Long.parseLong(content[4]));
+					int b;
+					for (long i=0; i<Long.parseLong(content[4]); i++) {
+						b = in.read();
+						System.out.print(b);
+					}
+					System.out.print("\n");
+					if (!in.readUTF().equals("Server: transfer complete")) throw new Exception("something went wrong");
+					break;
+				case "receiving":
+					// TODO: send file data to server
+					break;
+				}
 			} while (!message.equals("exit"));
+			socket.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
--- src/code/ConnectionThread.java
+++ src/code/ConnectionThread.java
@@ -3,30 +3,71 @@ package code;
 import java.net.Socket;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ConnectionThread extends Thread{
 	private Socket socket;
+	private DataInputStream in;
+	private DataOutputStream out;
+	private String h;
 	
-	public ConnectionThread (Socket socket) {
+	public ConnectionThread (Socket socket) throws IOException {
 		this.socket = socket;
+		this.in = new DataInputStream(this.socket.getInputStream());
+		this.out = new DataOutputStream(this.socket.getOutputStream());
+		this.h = "Server: ";//message header
+	}
+	
+	public void sendFile (String path) throws IOException, FileNotFoundException {
+    	File file = new File(path);
+    	//String[] pathArray = path.split("\\");
+    	//String filename = pathArray[pathArray.length - 1];
+    	out.writeUTF(this.h+"sending " + path + " size: " + file.length());
+    	out.flush();
+    	FileInputStream fis = new FileInputStream(file);
+    	int b;
+    	while ((b = fis.read()) != -1) {
+    		out.write(b);
+    		out.flush();
+    	}
+    	out.writeUTF(this.h+"transfer complete");
+    	out.flush();
+    	fis.close();
 	}
 	
 	public void run () {
 		try {
-			DataInputStream in = new DataInputStream(this.socket.getInputStream());
-			DataOutputStream out = new DataOutputStream(this.socket.getOutputStream());
 			String message;
-			String h = "Server: ";
+			String[] command;
 			while (true) {
 				message = in.readUTF();
 				if (message.equals("exit")) {
-					out.writeUTF(h+"bye");
+					out.writeUTF(this.h+"bye");
 					break;
 				}
-				out.writeUTF(h+"command not recognised");
+				command = message.split(" ");
+				switch (command[0]) {
+				case "download":
+					System.out.println("sending " + command[1]);
+					sendFile(command[1]);
+					System.out.println(command[1] + " sent");
+					break;
+				case "upload":
+					// TODO: receive file from client
+					break;
+				case "delete":
+					// TODO: delete specified file in server storage
+					break;
+				default:
+					out.writeUTF(this.h+"command not recognised");
+				}
 			}
 		} catch (Exception e) {
 			System.out.println(e);
+			e.printStackTrace();
 		}
 	}
 }
